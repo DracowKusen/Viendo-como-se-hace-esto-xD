149962180c57a4f04d5585c44ce8271b5b826be0
DracowKusen <dracowkusen@hotmail.com>
2025-01-30 00:34:09 -0600

    Agregado con if, elif y else

--- prueba_01.py
+++ prueba_01.py
@@ -1,1 +1,17 @@
+# Primer commit
+
+import os
+os.system("cls")
+
 print("Oal mundo")
+nombre = input("Como te llamas? ")
+print("Vale perfecto")
+edad = input("Cual es tu edad? ")
+print(f"Tu nombre es {nombre} y tienes {edad} años")
+input("Te gusta touhou? ")
+if input == "Si" or "si":
+    print("Le sabes :D")
+elif input == "No" or "no":
+    print("No le sabes :C")
+else:
+    print("Buscalo quizas te gustara ;)")
